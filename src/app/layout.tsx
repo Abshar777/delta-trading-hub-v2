f708e5a8a330730d1f9d16a140d2b5ff953a02f5
Abshar777 <absharameen625@gmail.com>
2026-08-18 17:01:30 +0530

    done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
         <Script id="gtm" strategy="beforeInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-T39R8T47');`}
         </Script>
+        <meta name="p:domain_verify" content="0993d6cf29c0d0c63730891dcd11461f"/>
       </head>
       <body>
         {/* ── Google Tag Manager (noscript fallback) ── */}
